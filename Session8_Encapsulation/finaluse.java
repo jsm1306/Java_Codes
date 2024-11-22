534e9b415a0eabcd22edebad54fda5c9ef50669e
jsm1306 <sindhu.j1729@gmail.com>
2024-11-22 16:47:22 +0530

    Added Session8

---
+++ Session8_Encapsulation/finaluse.java
@@ -0,0 +1,8 @@
+public class finaluse {
+    public static void main(String[] args) {
+        final int a=10;
+        // a=a*2;
+        System.out.println(a);
+        //will throw error
+    }
+}
